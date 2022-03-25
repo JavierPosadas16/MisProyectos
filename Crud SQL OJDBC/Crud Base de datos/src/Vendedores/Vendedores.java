bad0bae0c0efc68a2a1ab8cb41e7b0d7ff2b85af
javier Posadas <78783882+JavierPosadas16@users.noreply.github.com>
2022-03-25 14:22:52 -0600

    Proyectos

--- Crud SQL OJDBC/Crud Base de datos/src/Vendedores/Vendedores.java
+++ Crud SQL OJDBC/Crud Base de datos/src/Vendedores/Vendedores.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Vendedores;
 
-/**
- *
- * @author racco
- */
 public class Vendedores {
     private int id_vendedores;
     private String nombre;
--- Crud SQL OJDBC/Crud Base de datos/src/Vendedores/dao/EmpleadoDAO.java
+++ Crud SQL OJDBC/Crud Base de datos/src/Vendedores/dao/EmpleadoDAO.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Vendedores.dao;
 
 import Vendedores.Vendedores;
@@ -10,10 +6,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import javax.swing.JTable;
 
-/**
- *
- * @author racco
- */
 public class EmpleadoDAO {
     
     private String mensaje = "";
